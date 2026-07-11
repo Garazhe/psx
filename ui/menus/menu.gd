4ad286f8314c66eaae4cd7f2f1f9392eaf50b5a0
lurk <nikita.pvte@gmail.com>
2026-07-11 16:28:23 +0800

    переделал меню

--- ui/menus/menu.gd
+++ ui/menus/menu.gd
@@ -4,4 +4,8 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_play_pressed() -> void:
+	pass
+
+
+func _on_test_room_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/test_room/floor.tscn")
